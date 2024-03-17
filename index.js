c47ee93f51947b1e475cc8daf231023b70928e3a
VladimirVornikov <vornikov-vladimir@mail.ru>
2024-03-17 20:15:36 +0100

    17.03

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const telegramApi = require('node-telegram-bot-api');
-const express = require("express")
-const cors = require('cors')
 
-const app = express()
-
-app.use(express.json())
-app.use(cors())
 
 
 
@@ -40,28 +34,54 @@ bot.on("message", async (msg) => {
     }
     
     if(msg?.web_app_data?.data) {
+        console.log(msg);
         try {
             const data = JSON.parse(msg?.web_app_data?.data)
             if(data.totalTime > 0) {
                 bot.sendMessage(chatId, 
                 `You spent ${data.totalTime} ${data.totalTime > 1 ? "hours" : "hour"}`)
             }
+            bot.sendPhoto(chatId, data.picture);
         } catch(e) {
             console.log(e);
         }
     }
 })
 
-app.post("/postData", (req, res) => {
-    const postData = req.body;
-    console.log('Received data:', postData);
-    bot.sendPhoto(chatIdGlobal, `${postData.picture}`)
-    res.status(200).json({ message: 'Data received successfully.' });
+
+const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/upload', async (req, res) => {
+    try {
+        const { key, image, name } = req.body;
+
+        const response = await fetch('https://api.imgbb.com/1/upload', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({ key, image, name }),
+        });
+
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
-const PORT = 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.listen(PORT, () => console.log("server started on PORT " + PORT))
+
 
 
 
