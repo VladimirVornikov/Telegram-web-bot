3d26946d847e15561424c650a69123b78a8e0a01
VladimirVornikov <vornikov-vladimir@mail.ru>
2024-03-18 12:58:45 +0100

    18.03

--- index.js
+++ index.js
@@ -1,53 +1,8 @@
 const telegramApi = require('node-telegram-bot-api');
 
-
-
-
-const token = "7132665948:AAFECPqQmjvgjX30MWXi5ejJjRldEEdKHBY";
 const webAppUrl = 'https://telegramwebapptrood.netlify.app/'
-
+const token = "7132665948:AAFECPqQmjvgjX30MWXi5ejJjRldEEdKHBY";
 const bot = new telegramApi(token, {polling: true});
-
-let chatIdGlobal;
-
-bot.on("message", async (msg) => {
-    const text = msg.text;
-    const chatId = msg.chat.id;
-    const nickname = msg.from.first_name;
-    chatIdGlobal = chatId
-
-    if (text === "/start") {
-        await bot.sendMessage(chatId, `Hi ${nickname}, welcome to telegram bot. My name is TroodBot.`);
-        setTimeout(() => {
-            bot.sendMessage(chatId, "I'm a bot that helps people track time, and also take into account the user's health status based on their images.");
-        }, 1000);
-        
-        setTimeout(() => {
-            bot.sendMessage(chatId, "Please, fill the form below, and make a", {
-                reply_markup: {
-                    keyboard: [
-                        [{text: "Fill form", web_app: {url: webAppUrl}}]
-                    ]
-                }
-            });
-        }, 3000);   
-    }
-    
-    if(msg?.web_app_data?.data) {
-        console.log(msg);
-        try {
-            const data = JSON.parse(msg?.web_app_data?.data)
-            if(data.totalTime > 0) {
-                bot.sendMessage(chatId, 
-                `You spent ${data.totalTime} ${data.totalTime > 1 ? "hours" : "hour"}`)
-            }
-            bot.sendPhoto(chatId, data.picture);
-        } catch(e) {
-            console.log(e);
-        }
-    }
-})
-
 
 const cors = require('cors')
 const express = require('express');
@@ -59,9 +14,49 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(cors());
 
+bot.on("message", async (msg) => {
+    const text = msg.text;
+    const chatId = msg.chat.id;
+    const nickname = msg.from.first_name;
+
+    if (text === "/start") {
+        await bot.sendMessage(chatId, `Hi ${nickname}, welcome to telegram bot. My name is TroodBot.`);
+        setTimeout(() => {
+            bot.sendMessage(chatId, "I'm a bot that helps people track time, and also take into account the user's health status based on their images.");
+        }, 1000);
+        
+        setTimeout(() => {
+            bot.sendMessage(chatId, "Please provide the following information:");
+            setTimeout(() => {
+                bot.sendMessage(chatId, "What is your name, surname, year of birth? (Please use commas to separate data)");
+            }, 1000);
+        }, 3000);
+    } else if(msg.text != "/start") {
+        const userData = msg.text.split(",");
+        if(userData.length !== 3) {
+            bot.sendMessage(chatId, "Invalid input. Please provide your name, surname, and year of birth separated by commas.");
+            return;
+        }
+        
+        const [name, surname, year] = userData;
+        const age = 2024 - parseInt(year);
+        
+        if(isNaN(age) || age <= 0 ) {
+            bot.sendMessage(chatId, "Invalid year of birth. Please provide a valid year of birth.");
+            return;
+        }
+        
+        bot.sendMessage(chatId, `Are you ${surname} ${name} ${age} years old ?`);
+    }
+});
+
+
+
+
 app.post('/upload', async (req, res) => {
     try {
         const { key, image, name } = req.body;
+        console.log(`key: ${key} and name ${name}`);
 
         const response = await fetch('https://api.imgbb.com/1/upload', {
         method: 'POST',
@@ -80,9 +75,3 @@ app.post('/upload', async (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
-
-
-
